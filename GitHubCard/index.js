b431244ff0ebf9ee5791bc3754d66cdbe9cb2335
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-07-11 19:32:33 -0400

    almost done I think

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -5,6 +5,43 @@
 axios.get('https://api.github.com/users/cjmcnamee')
   .then(response => {
     console.log(response.data);
+    gitHubUser(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+axios.get('https://api.github.com/users/tetondan')
+  .then(response => {
+    console.log(response.data);
+    gitHubUser(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+axios.get('https://api.github.com/users/dustinmyers')
+  .then(response => {
+    console.log(response.data);
+    gitHubUser(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+axios.get('https://api.github.com/users/justsml')
+  .then(response => {
+    console.log(response.data);
+    gitHubUser(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+axios.get('https://api.github.com/users/luishrd')
+  .then(response => {
+    console.log(response.data);
+    gitHubUser(response.data);
   })
   .catch(err => {
     console.log(err);
@@ -53,35 +90,53 @@ const followersArray = [];
 
 */
 
-function gitHubUser(Obj){
+function gitHubUser(Obj) {
   // define new elements
   const divCard = document.createElement('div');
   const imgUrl = document.createElement('img');
   const divCardInfo = document.createElement('div');
-  const username = document.createElement('h3');
+  const h3Name = document.createElement('h3');
+  const pUsername = document.createElement('p');
   const pLocation = document.createElement('p');
   const pProfile = document.createElement('p');
-  const link = document.createElement('a');
+  const aLink = document.createElement('a');
   const pFollowers = document.createElement('p');
   const pFollowing = document.createElement('p');
   const pBio = document.createElement('p');
 
   // setup structure of elements
-  divCard.appendChild(imgUrl)
-  divCard.appendChild(divCardInfo)
-  divCardInfo.appendChild(username)
-  divCardInfo.appendChild(pLocation)
-  divCardInfo.appendChild(pProfile)
-  pProfileInfo.appendChild(link)
-  divCardInfo.appendChild(pFollowers)
-  divCardInfo.appendChild(pFollowing)
-  divCardInfo.appendChild(pBio)
+  divCard.appendChild(imgUrl);
+  divCard.appendChild(divCardInfo);
+  divCardInfo.appendChild(h3Name);
+  divCardInfo.appendChild(pUsername);
+  divCardInfo.appendChild(pLocation);
+  divCardInfo.appendChild(pProfile);
+  pProfile.appendChild(aLink);
+  divCardInfo.appendChild(pFollowers);
+  divCardInfo.appendChild(pFollowing);
+  divCardInfo.appendChild(pBio);
 
   // set class names
-  divCard.classList.add('card')
-  divCardInfo.classList.add('card-info')
-  username.classList.add('username')
+  divCard.classList.add('card');
+  divCardInfo.classList.add('card-info');
+  h3Name.classList.add('name');
+  pUsername.classList.add('username');
+
+  // set text contents
+  imgUrl.src = Obj.avatar_url;
+  h3Name.innerHTML = Obj.name;
+  pUsername.innerHTML = Obj.login;
+  pLocation.innerHTML = `Location: ${Obj.location}`;
+  pProfile.innerHTML = "Profile: ";
+  aLink.href = Obj.html_url;
+  aLink.innerHTML = Obj.html_url;
+  pFollowers.innerHTML = `Followers: ${Obj.followers_url}`;
+  pFollowing.innerHTML = `Following: ${Obj.following_url}`;
+  pBio.innerHTML = `Bio: ${Obj.bio}`;
+
+  console.log(divCard)
 }
+
 
 /* List of LS Instructors Github username's:
   tetondan
